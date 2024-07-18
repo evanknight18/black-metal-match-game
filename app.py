a177474fd2bee686a69a565c6dc2eeff16377aa3
Evan Knight <evanknight18@gmail.com>
2024-07-18 15:30:46 -0600

    Fix port binding and initialize Pygame mixer

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import threading
+import os
 import game
+import pygame
 
 app = Flask(__name__)
 
@@ -9,8 +11,10 @@ def index():
     return render_template('index.html')
 
 def run_game():
+    pygame.mixer.init()  # Initialize the Pygame mixer
     game.main()
 
 if __name__ == "__main__":
     threading.Thread(target=run_game).start()
-    app.run(debug=True, use_reloader=False, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))  # Use the port specified by Heroku
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=port)
